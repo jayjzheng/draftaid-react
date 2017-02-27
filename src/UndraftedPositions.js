4db256f98d1065d12e147218b2adc7aa6e590235
Jay Zheng <jay.j.zheng@gmail.com>
2017-02-26 23:48:42 -0800

    minor clean up

--- src/UndraftedPositions.js
+++ src/UndraftedPositions.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Undrafted from './Undrafted'
 
 function UndraftedPositions(props) {
+  const fields = ['tier', 'name', 'team', 'bye_week'];
+
   return (
     <div className='col-md-6 col-sm-12 hidden-xs'>
       <div className='aid-title'>
@@ -12,7 +14,7 @@ function UndraftedPositions(props) {
       <div className='col-sm-6'>
         <span className="col-sm-12 position-title">Runningbacks</span>
         <Undrafted
-          fields={['tier', 'name', 'team', 'bye_week']}
+          fields={fields}
           players={props.players}
           draft={(p) => props.draft(p)}
           size={15}
@@ -23,7 +25,7 @@ function UndraftedPositions(props) {
       <div className='col-sm-6'>
         <span className="col-sm-12 position-title">Wide Receivers</span>
         <Undrafted
-          fields={['tier', 'name', 'team', 'bye_week']}
+          fields={fields}
           players={props.players}
           draft={(p) => props.draft(p)}
           size={15}
@@ -34,7 +36,7 @@ function UndraftedPositions(props) {
       <div className='col-sm-6'>
         <span className="col-sm-12 position-title">Quarterbacks</span>
         <Undrafted
-          fields={['tier', 'name', 'team', 'bye_week']}
+          fields={fields}
           players={props.players}
           draft={(p) => props.draft(p)}
           size={15}
@@ -45,7 +47,7 @@ function UndraftedPositions(props) {
       <div className='col-sm-6'>
         <span className="col-sm-12 position-title">Tightends</span>
         <Undrafted
-          fields={['tier', 'name', 'team', 'bye_week']}
+          fields={fields}
           players={props.players}
           draft={(p) => props.draft(p)}
           size={15}
